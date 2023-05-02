6b79dbcd53166d552e13534e3af74d1ba0f7fadc
Matt Hunt <matt@zero.co.uk>
2023-05-02 10:34:50 +0100

    Adding TypedFeatureCollection

--- src/GeoJSON.Text.Test.Unit/Feature/FeatureCollectionTests.cs
+++ src/GeoJSON.Text.Test.Unit/Feature/FeatureCollectionTests.cs
@@ -6,169 +6,189 @@ using GeoJSON.Text.Feature;
 using GeoJSON.Text.Geometry;
 using NUnit.Framework;
 
-namespace GeoJSON.Text.Tests.Feature
+namespace GeoJSON.Text.Tests.Feature;
+
+[TestFixture]
+public class FeatureCollectionTests : TestBase
 {
-    [TestFixture]
-    public class FeatureCollectionTests : TestBase
+    [Test]
+    public void Ctor_Throws_ArgumentNullException_When_Features_Is_Null()
     {
-        [Test]
-        public void Ctor_Throws_ArgumentNullException_When_Features_Is_Null()
+        Assert.Throws<ArgumentNullException>(() =>
         {
-            Assert.Throws<ArgumentNullException>(() =>
+            var featureCollection = new FeatureCollection(null);
+        });
+    }
+
+    [Test]
+    public void Can_Deserialize()
+    {
+        string json = GetExpectedJson();
+
+        var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json);
+
+        Assert.IsNotNull(featureCollection);
+        Assert.IsNotNull(featureCollection.Features);
+        Assert.AreEqual(featureCollection.Features.Count, 3);
+        Assert.AreEqual(featureCollection.Features.Count(x => x.Geometry.Type == GeoJSONObjectType.Point), 1);
+        Assert.AreEqual(featureCollection.Features.Count(x => x.Geometry.Type == GeoJSONObjectType.MultiPolygon), 1);
+        Assert.AreEqual(featureCollection.Features.Count(x => x.Geometry.Type == GeoJSONObjectType.Polygon), 1);
+    }
+
+    [Test]
+    public void Can_DeserializeGeneric()
+    {
+        string json = GetExpectedJson();
+
+        var featureCollection =
+            JsonSerializer.Deserialize<FeatureCollection<FeatureCollectionTestPropertyObject>>(json);
+
+        Assert.IsNotNull(featureCollection);
+        Assert.IsNotNull(featureCollection.Features);
+        Assert.AreEqual(featureCollection.Features.Count, 3);
+        Assert.AreEqual("DD", featureCollection.Features.First().Properties.Name);
+        Assert.AreEqual(123, featureCollection.Features.First().Properties.Size);
+    }
+
+    [Test]
+    public void FeatureCollectionSerialization()
+    {
+        var model = new FeatureCollection();
+        for (var i = 10; i-- > 0;)
+        {
+            var geom = new LineString(new[]
             {
-                var featureCollection = new FeatureCollection(null);
+                new Position(51.010, -1.034),
+                new Position(51.010, -0.034)
             });
-        }
 
-        [Test]
-        public void Can_Deserialize()
-        {
-            string json = GetExpectedJson();
-
-            var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json);
-
-            Assert.IsNotNull(featureCollection.Features);
-            Assert.AreEqual(featureCollection.Features.Count, 3);
-            Assert.AreEqual(featureCollection.Features.Count(x => x.Geometry.Type == GeoJSONObjectType.Point), 1);
-            Assert.AreEqual(featureCollection.Features.Count(x => x.Geometry.Type == GeoJSONObjectType.MultiPolygon), 1);
-            Assert.AreEqual(featureCollection.Features.Count(x => x.Geometry.Type == GeoJSONObjectType.Polygon), 1);
-        }
-
-        [Test]
-        public void FeatureCollectionSerialization()
-        {
-            var model = new FeatureCollection();
-            for (var i = 10; i-- > 0;)
+            var props = new Dictionary<string, object>
             {
-                var geom = new LineString(new[]
-                {
-                    new Position(51.010, -1.034),
-                    new Position(51.010, -0.034)
-                });
+                { "test1", "1" },
+                { "test2", 2 }
+            };
 
-                var props = new Dictionary<string, object>
-                {
-                    { "test1", "1" },
-                    { "test2", 2 }
-                };
-
-                var feature = new Text.Feature.Feature(geom, props);
-                model.Features.Add(feature);
-            }
-
-            var actualJson = JsonSerializer.Serialize(model);
-
-            Assert.IsNotNull(actualJson);
-
-            Assert.IsFalse(string.IsNullOrEmpty(actualJson));
+            var feature = new Text.Feature.Feature(geom, props);
+            model.Features.Add(feature);
         }
 
-        [Test]
-        public void FeatureCollection_Equals_GetHashCode_Contract()
+        var actualJson = JsonSerializer.Serialize(model);
+
+        Assert.IsNotNull(actualJson);
+
+        Assert.IsFalse(string.IsNullOrEmpty(actualJson));
+    }
+
+    [Test]
+    public void FeatureCollection_Equals_GetHashCode_Contract()
+    {
+        var left = GetFeatureCollection();
+        var right = GetFeatureCollection();
+
+        Assert_Are_Equal(left, right);
+    }
+
+    [Test]
+    public void Serialized_And_Deserialized_FeatureCollection_Equals_And_Share_HashCode()
+    {
+        var leftFc = GetFeatureCollection();
+        var leftJson = JsonSerializer.Serialize(leftFc);
+        var left = JsonSerializer.Deserialize<FeatureCollection>(leftJson);
+
+        var rightFc = GetFeatureCollection();
+        var rightJson = JsonSerializer.Serialize(rightFc);
+        var right = JsonSerializer.Deserialize<FeatureCollection>(rightJson);
+
+        Assert_Are_Equal(left, right);
+    }
+
+    [Test]
+    public void FeatureCollection_Test_IndexOf()
+    {
+        var model = new FeatureCollection();
+        var expectedIds = new List<string>();
+        var expectedIndexes = new List<int>();
+
+        for (var i = 0; i < 10; i++)
         {
-            var left = GetFeatureCollection();
-            var right = GetFeatureCollection();
+            var id = "id" + i;
 
-            Assert_Are_Equal(left, right);
-        }
+            expectedIds.Add(id);
+            expectedIndexes.Add(i);
 
-        [Test]
-        public void Serialized_And_Deserialized_FeatureCollection_Equals_And_Share_HashCode()
-        {
-            var leftFc = GetFeatureCollection();
-            var leftJson = JsonSerializer.Serialize(leftFc);
-            var left = JsonSerializer.Deserialize<FeatureCollection>(leftJson);
-
-            var rightFc = GetFeatureCollection();
-            var rightJson = JsonSerializer.Serialize(rightFc);
-            var right = JsonSerializer.Deserialize<FeatureCollection>(rightJson);
-
-            Assert_Are_Equal(left, right);
-        }
-
-        [Test]
-        public void FeatureCollection_Test_IndexOf()
-        {
-            var model = new FeatureCollection();
-            var expectedIds = new List<string>();
-            var expectedIndexes = new List<int>();
-
-            for (var i = 0; i < 10; i++)
+            var geom = new LineString(new[]
             {
-                var id = "id" + i;
+                new Position(51.010, -1.034),
+                new Position(51.010, -0.034)
+            });
 
-                expectedIds.Add(id);
-                expectedIndexes.Add(i);
+            var props = FeatureTests.GetPropertiesInRandomOrder();
 
-                var geom = new LineString(new[]
-                {
-                    new Position(51.010, -1.034),
-                    new Position(51.010, -0.034)
-                });
-
-                var props = FeatureTests.GetPropertiesInRandomOrder();
-
-                var feature = new Text.Feature.Feature(geom, props, id);
-                model.Features.Add(feature);
-            }
-
-            for (var i = 0; i < 10; i++)
-            {
-                var actualFeature = model.Features[i];
-                var actualId = actualFeature.Id;
-                var actualIndex = model.Features.IndexOf(actualFeature);
-
-                var expectedId = expectedIds[i];
-                var expectedIndex = expectedIndexes[i];
-
-                Assert.AreEqual(expectedId, actualId);
-                Assert.AreEqual(expectedIndex, actualIndex);
-
-                Assert.Inconclusive("not supported. the Feature.Id is optional. " +
-                    " create a new class that inherits from" +
-                    " Feature and then override Equals and GetHashCode");
-
-            }
-
+            var feature = new Text.Feature.Feature(geom, props, id);
+            model.Features.Add(feature);
         }
 
-
-        private FeatureCollection GetFeatureCollection()
+        for (var i = 0; i < 10; i++)
         {
-            var model = new FeatureCollection();
-            for (var i = 10; i-- > 0;)
-            {
-                var geom = new LineString(new[]
-                {
-                    new Position(51.010, -1.034),
-                    new Position(51.010, -0.034)
-                });
+            var actualFeature = model.Features[i];
+            var actualId = actualFeature.Id;
+            var actualIndex = model.Features.IndexOf(actualFeature);
 
-                var props = FeatureTests.GetPropertiesInRandomOrder();
+            var expectedId = expectedIds[i];
+            var expectedIndex = expectedIndexes[i];
 
-                var feature = new Text.Feature.Feature(geom, props);
-                model.Features.Add(feature);
-            }
-            return model;
+            Assert.AreEqual(expectedId, actualId);
+            Assert.AreEqual(expectedIndex, actualIndex);
+
+            Assert.Inconclusive("not supported. the Feature.Id is optional. " +
+                                " create a new class that inherits from" +
+                                " Feature and then override Equals and GetHashCode");
         }
+    }
 
-        private void Assert_Are_Equal(FeatureCollection left, FeatureCollection right)
+
+    private FeatureCollection GetFeatureCollection()
+    {
+        var model = new FeatureCollection();
+        for (var i = 10; i-- > 0;)
         {
-            Assert.AreEqual(left, right);
+            var geom = new LineString(new[]
+            {
+                new Position(51.010, -1.034),
+                new Position(51.010, -0.034)
+            });
 
-            Assert.IsTrue(left.Equals(right));
-            Assert.IsTrue(right.Equals(left));
+            var props = FeatureTests.GetPropertiesInRandomOrder();
 
-            Assert.IsTrue(left.Equals(left));
-            Assert.IsTrue(right.Equals(right));
-
-            Assert.IsTrue(left == right);
-            Assert.IsTrue(right == left);
-
-            Assert.IsFalse(left != right);
-            Assert.IsFalse(right != left);
-
-            Assert.AreEqual(left.GetHashCode(), right.GetHashCode());
+            var feature = new Text.Feature.Feature(geom, props);
+            model.Features.Add(feature);
         }
+
+        return model;
+    }
+
+    private void Assert_Are_Equal(FeatureCollection left, FeatureCollection right)
+    {
+        Assert.AreEqual(left, right);
+
+        Assert.IsTrue(left.Equals(right));
+        Assert.IsTrue(right.Equals(left));
+
+        Assert.IsTrue(left.Equals(left));
+        Assert.IsTrue(right.Equals(right));
+
+        Assert.IsTrue(left == right);
+        Assert.IsTrue(right == left);
+
+        Assert.IsFalse(left != right);
+        Assert.IsFalse(right != left);
+
+        Assert.AreEqual(left.GetHashCode(), right.GetHashCode());
+    }
+
+    private class FeatureCollectionTestPropertyObject
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
     }
 }
--- src/GeoJSON.Text/Feature/FeatureCollection.cs
+++ src/GeoJSON.Text/Feature/FeatureCollection.cs
@@ -5,123 +5,245 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
+using GeoJSON.Text.Geometry;
 
-namespace GeoJSON.Text.Feature
+namespace GeoJSON.Text.Feature;
+
+/// <summary>
+/// Defines the FeatureCollection type.
+/// </summary>
+public class FeatureCollection : GeoJSONObject, IEqualityComparer<FeatureCollection>, IEquatable<FeatureCollection>
 {
     /// <summary>
-    /// Defines the FeatureCollection type.
+    /// Initializes a new instance of the <see cref="FeatureCollection" /> class.
     /// </summary>
-    public class FeatureCollection : GeoJSONObject, IEqualityComparer<FeatureCollection>, IEquatable<FeatureCollection>
+    public FeatureCollection() : this(new List<Feature>())
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FeatureCollection" /> class.
-        /// </summary>
-        public FeatureCollection() : this(new List<Feature>())
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FeatureCollection" /> class.
+    /// </summary>
+    /// <param name="features">The features.</param>
+    public FeatureCollection(List<Feature> features)
+    {
+        if (features == null)
         {
+            throw new ArgumentNullException(nameof(features));
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FeatureCollection" /> class.
-        /// </summary>
-        /// <param name="features">The features.</param>
-        public FeatureCollection(List<Feature> features)
-        {
-            if (features == null)
-            {
-                throw new ArgumentNullException(nameof(features));
-            }
+        Features = features;
+    }
 
-            Features = features;
+    [JsonPropertyName("type")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public override GeoJSONObjectType Type => GeoJSONObjectType.FeatureCollection;
+
+    /// <summary>
+    /// Gets the features.
+    /// </summary>
+    /// <value>The features.</value>
+    [JsonPropertyName("features")]
+    //[JsonConverter(typeof(FeatureEnumerableConverter))]
+    public List<Feature> Features { get; set; }
+
+    #region IEqualityComparer, IEquatable
+
+    /// <summary>
+    /// Determines whether the specified object is equal to the current object
+    /// </summary>
+    public override bool Equals(object obj)
+    {
+        return Equals(this, obj as FeatureCollection);
+    }
+
+    /// <summary>
+    /// Determines whether the specified object is equal to the current object
+    /// </summary>
+    public bool Equals(FeatureCollection other)
+    {
+        return Equals(this, other);
+    }
+
+    /// <summary>
+    /// Determines whether the specified object instances are considered equal
+    /// </summary>
+    public bool Equals(FeatureCollection left, FeatureCollection right)
+    {
+        if (base.Equals(left, right))
+        {
+            return left.Features.SequenceEqual(right.Features);
         }
 
-        [JsonPropertyName("type")]
-        [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
-        [JsonConverter(typeof(JsonStringEnumConverter))]
-        public override GeoJSONObjectType Type => GeoJSONObjectType.FeatureCollection;
+        return false;
+    }
 
-        /// <summary>
-        /// Gets the features.
-        /// </summary>
-        /// <value>The features.</value>
-        [JsonPropertyName("features")]
-        //[JsonConverter(typeof(FeatureEnumerableConverter))]
-        public List<Feature> Features { get; set; }
-
-        #region IEqualityComparer, IEquatable
-
-        /// <summary>
-        /// Determines whether the specified object is equal to the current object
-        /// </summary>
-        public override bool Equals(object obj)
+    /// <summary>
+    /// Determines whether the specified object instances are considered equal
+    /// </summary>
+    public static bool operator ==(FeatureCollection left, FeatureCollection right)
+    {
+        if (ReferenceEquals(left, right))
         {
-            return Equals(this, obj as FeatureCollection);
+            return true;
         }
 
-        /// <summary>
-        /// Determines whether the specified object is equal to the current object
-        /// </summary>
-        public bool Equals(FeatureCollection other)
+        if (ReferenceEquals(null, right))
         {
-            return Equals(this, other);
-        }
-
-        /// <summary>
-        /// Determines whether the specified object instances are considered equal
-        /// </summary>
-        public bool Equals(FeatureCollection left, FeatureCollection right)
-        {
-            if (base.Equals(left, right))
-            {
-                return left.Features.SequenceEqual(right.Features);
-            }
             return false;
         }
 
-        /// <summary>
-        /// Determines whether the specified object instances are considered equal
-        /// </summary>
-        public static bool operator ==(FeatureCollection left, FeatureCollection right)
-        {
-            if (ReferenceEquals(left, right))
-            {
-                return true;
-            }
-            if (ReferenceEquals(null, right))
-            {
-                return false;
-            }
-            return left != null && left.Equals(right);
-        }
-
-        /// <summary>
-        /// Determines whether the specified object instances are not considered equal
-        /// </summary>
-        public static bool operator !=(FeatureCollection left, FeatureCollection right)
-        {
-            return !(left == right);
-        }
-
-        /// <summary>
-        /// Returns the hash code for this instance
-        /// </summary>
-        public override int GetHashCode()
-        {
-            int hash = base.GetHashCode();
-            foreach (var feature in Features)
-            {
-                hash = (hash * 397) ^ feature.GetHashCode();
-            }
-            return hash;
-        }
-
-        /// <summary>
-        /// Returns the hash code for the specified object
-        /// </summary>
-        public int GetHashCode(FeatureCollection other)
-        {
-            return other.GetHashCode();
-        }
-
-        #endregion
+        return left != null && left.Equals(right);
     }
+
+    /// <summary>
+    /// Determines whether the specified object instances are not considered equal
+    /// </summary>
+    public static bool operator !=(FeatureCollection left, FeatureCollection right)
+    {
+        return !(left == right);
+    }
+
+    /// <summary>
+    /// Returns the hash code for this instance
+    /// </summary>
+    public override int GetHashCode()
+    {
+        int hash = base.GetHashCode();
+        foreach (var feature in Features)
+        {
+            hash = (hash * 397) ^ feature.GetHashCode();
+        }
+
+        return hash;
+    }
+
+    /// <summary>
+    /// Returns the hash code for the specified object
+    /// </summary>
+    public int GetHashCode(FeatureCollection other)
+    {
+        return other.GetHashCode();
+    }
+
+    #endregion
+}
+
+public class FeatureCollection<TProps> : FeatureCollection, IEqualityComparer<FeatureCollection<TProps>>,
+    IEquatable<FeatureCollection<TProps>>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FeatureCollection" /> class.
+    /// </summary>
+    public FeatureCollection() : this(new List<Feature<IGeometryObject, TProps>>())
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FeatureCollection" /> class.
+    /// </summary>
+    /// <param name="features">The features.</param>
+    public FeatureCollection(List<Feature<IGeometryObject, TProps>> features)
+    {
+        if (features == null)
+        {
+            throw new ArgumentNullException(nameof(features));
+        }
+
+        Features = features;
+    }
+
+    [JsonPropertyName("type")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public override GeoJSONObjectType Type => GeoJSONObjectType.FeatureCollection;
+
+    /// <summary>
+    /// Gets the features.
+    /// </summary>
+    /// <value>The features.</value>
+    [JsonPropertyName("features")]
+    new public List<Feature<IGeometryObject, TProps>> Features { get; private set; }
+
+    #region IEqualityComparer, IEquatable
+
+    /// <summary>
+    /// Determines whether the specified object is equal to the current object
+    /// </summary>
+    public override bool Equals(object obj)
+    {
+        return Equals(this, obj as FeatureCollection<TProps>);
+    }
+
+    /// <summary>
+    /// Determines whether the specified object is equal to the current object
+    /// </summary>
+    public bool Equals(FeatureCollection<TProps> other)
+    {
+        return Equals(this, other);
+    }
+
+    /// <summary>
+    /// Determines whether the specified object instances are considered equal
+    /// </summary>
+    public bool Equals(FeatureCollection<TProps> left, FeatureCollection<TProps> right)
+    {
+        if (base.Equals(left, right))
+        {
+            return left.Features.SequenceEqual(right.Features);
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Determines whether the specified object instances are considered equal
+    /// </summary>
+    public static bool operator ==(FeatureCollection<TProps> left, FeatureCollection<TProps> right)
+    {
+        if (ReferenceEquals(left, right))
+        {
+            return true;
+        }
+
+        if (ReferenceEquals(null, right))
+        {
+            return false;
+        }
+
+        return left != null && left.Equals(right);
+    }
+
+    /// <summary>
+    /// Determines whether the specified object instances are not considered equal
+    /// </summary>
+    public static bool operator !=(FeatureCollection<TProps> left, FeatureCollection<TProps> right)
+    {
+        return !(left == right);
+    }
+
+    /// <summary>
+    /// Returns the hash code for this instance
+    /// </summary>
+    public override int GetHashCode()
+    {
+        int hash = base.GetHashCode();
+        foreach (var feature in Features)
+        {
+            hash = (hash * 397) ^ feature.GetHashCode();
+        }
+
+        return hash;
+    }
+
+    /// <summary>
+    /// Returns the hash code for the specified object
+    /// </summary>
+    public int GetHashCode(FeatureCollection<TProps> other)
+    {
+        return other.GetHashCode();
+    }
+
+    #endregion
 }
